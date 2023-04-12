ad812dee8b1c4246831d62ee443dfaed5c00febd
PrashantSahu29 <prashant.sahu@incedoinc.com>
2023-04-12 12:21:15 +0530

    clear button

--- my-app/src/ClaimForm.jsx
+++ my-app/src/ClaimForm.jsx
@@ -50,50 +50,57 @@ const ExpenseClaimForm = () => {
       // window.alert("Form submitted successfully!"); // Show the alert popup
     }
   };
+  
+  const handleClear = () => {
+    setCategory('');
+    setDescription('');
+    setReceiptDate('');
+    setClaimAmount('');
+    setIsSubmitted(false);
+  }
 
   return (
-    <>
     <div className="expense-claim-form-container" style={{ backgroundImage: `url(${ClaimImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
       <div>
-      {/* <h1 style={{marginLeft:"20px"}}>Expense Claim Form</h1> */}
-      <form onSubmit={handleSubmit} className="expense-claim-form">
-        <h2 class="text-center" style={{paddingBottom:"20px"}}>Expense Claim Form</h2>
-        <div className="form-row">
-          <label htmlFor="category">Category:</label>
-          <select id="category" value={category} onChange={(e) => setCategory(e.target.value)}>
-            <option value="">Select category</option>
-            {categories.map((item, index) => (
-              <option key={index} value={item.name}>
-                {item.name}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="form-row">
-          <label htmlFor="description">Claim Description:</label>
-          <textarea
-            id="description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            maxLength={200}
-            style={{resize: "none"}}
-          />
-        </div>
-        <div className="form-row">
-          <label htmlFor="receipt-date">Receipt Date:</label>
-          <input  type="date" id="receipt-date" value={receiptDate} onChange={(e) => setReceiptDate(e.target.value)} />
-        </div>
-        <div className="form-row">
-          <label htmlFor="claim-amount">Claim Amount:</label>
-          <input type="number" step="0.01" id="claim-amount" value={claimAmount} onChange={(e) => setClaimAmount(e.target.value)} />
-        </div>
-        {error && <p className="error">{error}</p>}
-        {isSubmitted && <p className="success">Form submitted successfully!</p>}
-        <button type="submit" style={{margin:"auto", marginLeft:"38%"}}>Submit</button>
-      </form>
+        {/* <h1 style={{marginLeft:"20px"}}>Expense Claim Form</h1> */}
+        <form onSubmit={handleSubmit} className="expense-claim-form">
+          <h2 class="text-center" style={{paddingBottom:"20px"}}>Expense Claim Form</h2>
+          <div className="form-row">
+            <label htmlFor="category">Category:</label>
+            <select id="category" value={category} onChange={(e) => setCategory(e.target.value)}>
+              <option value="">Select category</option>
+              {categories.map((item, index) => (
+                <option key={index} value={item.name}>
+                  {item.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="form-row">
+            <label htmlFor="description">Claim Description:</label>
+            <textarea
+              id="description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              maxLength={200}
+              style={{resize: "none"}}
+            />
+          </div>
+          <div className="form-row">
+            <label htmlFor="receipt-date">Receipt Date:</label>
+            <input  type="date" id="receipt-date" value={receiptDate} onChange={(e) => setReceiptDate(e.target.value)} />
+          </div>
+          <div className="form-row">
+            <label htmlFor="claim-amount">Claim Amount:</label>
+            <input type="number" step="0.01" id="claim-amount" value={claimAmount} onChange={(e) => setClaimAmount(e.target.value)} />
+          </div>
+          {error && <p className="error">{error}</p>}
+          {isSubmitted && <p className="success">Form submitted successfully!</p>}
+          <button type="submit" style={{margin:"auto", marginLeft:"38%"}}>Submit</button>
+           <button type="reset" onClick={handleClear} style={{marginLeft:"20px"}}>Clear</button>
+        </form>
       </div>
     </div>
-    </>
   );
 };
 
