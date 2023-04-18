a406a442bad1c34a05d92ad6188f37b38d445c5e
PrashantSahu29 <prashant.sahu@incedoinc.com>
2023-04-18 23:04:14 +0530

    .

--- my-app/src/ClaimForm.jsx
+++ my-app/src/ClaimForm.jsx
@@ -15,13 +15,13 @@ const categories = [
 
 const ExpenseClaimForm = () => {
   // Declare state variables using the 'useState' hook
-const [category, setCategory] = useState('');
-const [description, setDescription] = useState('');
-const [descriptionLength, setDescriptionLength] = useState(0); // Track the length of the description input
-const [receiptDate, setReceiptDate] = useState('');
-const [claimAmount, setClaimAmount] = useState('');
-const [error, setError] = useState('');
-const [isSubmitted, setIsSubmitted] = useState(false);
+  const [category, setCategory] = useState('');
+  const [description, setDescription] = useState('');
+  const [descriptionLength, setDescriptionLength] = useState(0); // Track the length of the description input
+  const [receiptDate, setReceiptDate] = useState('');
+  const [claimAmount, setClaimAmount] = useState('');
+  const [error, setError] = useState('');
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
 
   const handleClear = () => {
@@ -35,88 +35,88 @@ const [isSubmitted, setIsSubmitted] = useState(false);
   };
 
   // Define a function that handles form submission
-const handleSubmit = (event) => {
-  event.preventDefault(); // Prevent the default form submission behavior
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
 
-  // Find the selected category from the list of categories
-  const selectedCategory = categories.find((item) => item.name === category);
+    // Find the selected category from the list of categories
+    const selectedCategory = categories.find((item) => item.name === category);
 
-  // Validate Form Inputs
-  if (!category || !description || !receiptDate || !claimAmount) {
-    // Display an error message if any required fields are empty
-    toast.error ('Please fill in all required fields.');
-    return;
-  } else if (!selectedCategory) {
-    // Display an error message if the selected category is invalid
-    toast.error('Invalid category.');
-  } else if (parseFloat(claimAmount) > selectedCategory.maxAmount) {
-    // Display an error message if the claim amount exceeds the maximum allowed for the selected category
-    toast.error('Claim amount exceeds the maximum allowed for this category.');
-  } else if (
-    isNaN(Date.parse(receiptDate)) ||
-    new Date(receiptDate).getTime() > Date.now() ||
-    (Date.now() - new Date(receiptDate).getTime()) / (24 * 60 * 60 * 1000) > 30
-  ) {
-    // Display an error message if the receipt date is invalid or exceeds the maximum allowed duration (30 days)
-    toast.error('Receipt date is invalid or exceeds the maximum allowed duration.');
-  } else if (descriptionLength > 200) {
-    // Display an error message if the claim description exceeds the maximum character limit (200 characters)
-    toast.error('Claim description exceeds the maximum character limit.');
-  } else {
-    // If all input is valid, log the form data to the console, set isSubmitted to true to trigger a success message, reset the form, and clear any error messages
-    console.log({ category, description, receiptDate, claimAmount });
-    setIsSubmitted(true);
-    setError('');
-    event.target.reset(); // Reset the form
-    setIsSubmitted(false); // Set isSubmitted back to false
-    // Show a success message using Sweet Alert
-    Swal.fire({
-      icon: 'success',
-      title: 'Form submitted successfully!',
-    });
-    handleClear();
-  }
-};
+    // Validate Form Inputs
+    if (!category || !description || !receiptDate || !claimAmount) {
+      // Display an error message if any required fields are empty
+      toast.error('Please fill in all required fields.');
+      return;
+    } else if (!selectedCategory) {
+      // Display an error message if the selected category is invalid
+      toast.error('Invalid category.');
+    } else if (parseFloat(claimAmount) > selectedCategory.maxAmount) {
+      // Display an error message if the claim amount exceeds the maximum allowed for the selected category
+      toast.error('Claim amount exceeds the maximum allowed for this category.');
+    } else if (
+      isNaN(Date.parse(receiptDate)) ||
+      new Date(receiptDate).getTime() > Date.now() ||
+      (Date.now() - new Date(receiptDate).getTime()) / (24 * 60 * 60 * 1000) > 30
+    ) {
+      // Display an error message if the receipt date is invalid or exceeds the maximum allowed duration (30 days)
+      toast.error('Receipt date is invalid or exceeds the maximum allowed duration.');
+    } else if (descriptionLength > 200) {
+      // Display an error message if the claim description exceeds the maximum character limit (200 characters)
+      toast.error('Claim description exceeds the maximum character limit.');
+    } else {
+      // If all input is valid, log the form data to the console, set isSubmitted to true to trigger a success message, reset the form, and clear any error messages
+      console.log({ category, description, receiptDate, claimAmount });
+      setIsSubmitted(true);
+      setError('');
+      event.target.reset(); // Reset the form
+      setIsSubmitted(false); // Set isSubmitted back to false
+      // Show a success message using Sweet Alert
+      Swal.fire({
+        icon: 'success',
+        title: 'Form submitted successfully!',
+      });
+      handleClear();
+    }
+  };
 
 
   // Function to get the current date in the format YYYY-MM-DD
-const getCurrentDate = () => {
-  const today = new Date();
-  const yyyy = today.getFullYear();
-  let mm = today.getMonth() + 1; // getMonth() returns 0-indexed month, so add 1 to get the correct month number
-  let dd = today.getDate();
+  const getCurrentDate = () => {
+    const today = new Date();
+    const yyyy = today.getFullYear();
+    let mm = today.getMonth() + 1; // getMonth() returns 0-indexed month, so add 1 to get the correct month number
+    let dd = today.getDate();
 
-  // Add leading zero to single digit date or month
-  if (dd < 10) {
-    dd = `0${dd}`;
-  }
+    // Add leading zero to single digit date or month
+    if (dd < 10) {
+      dd = `0${dd}`;
+    }
 
-  if (mm < 10) {
-    mm = `0${mm}`;
-  }
+    if (mm < 10) {
+      mm = `0${mm}`;
+    }
 
-  return `${yyyy}-${mm}-${dd}`;
-};
+    return `${yyyy}-${mm}-${dd}`;
+  };
 
-// Function to get the minimum date allowed, which is 30 days ago from the current date
-const getMinDate = () => {
-  const today = new Date();
-  const thirtyDaysAgo = new Date(today.setDate(today.getDate() - 30)); // Set the date to 30 days ago
-  const yyyy = thirtyDaysAgo.getFullYear();
-  let mm = thirtyDaysAgo.getMonth() + 1; // getMonth() returns 0-indexed month, so add 1 to get the correct month number
-  let dd = thirtyDaysAgo.getDate();
+  // Function to get the minimum date allowed, which is 30 days ago from the current date
+  const getMinDate = () => {
+    const today = new Date();
+    const thirtyDaysAgo = new Date(today.setDate(today.getDate() - 30)); // Set the date to 30 days ago
+    const yyyy = thirtyDaysAgo.getFullYear();
+    let mm = thirtyDaysAgo.getMonth() + 1; // getMonth() returns 0-indexed month, so add 1 to get the correct month number
+    let dd = thirtyDaysAgo.getDate();
 
-  // Add leading zero to single digit date or month
-  if (dd < 10) {
-    dd = `0${dd}`;
-  }
+    // Add leading zero to single digit date or month
+    if (dd < 10) {
+      dd = `0${dd}`;
+    }
 
-  if (mm < 10) {
-    mm = `0${mm}`;
-  }
+    if (mm < 10) {
+      mm = `0${mm}`;
+    }
 
-  return `${yyyy}-${mm}-${dd}`;
-};
+    return `${yyyy}-${mm}-${dd}`;
+  };
 
 
   const handleDescriptionChange = (event) => {
@@ -144,15 +144,7 @@ const getMinDate = () => {
               ))}
             </select>
           </div>
-          <div className="form-row">
-            <label htmlFor="description">Claim Description:</label>
-            {/* Textarea to input the description of the expense claim */}
-            <textarea id="description" value={description} onChange={handleDescriptionChange} maxLength={200} placeholder="Eg. This is description" />
-          </div>
-          {/* Show the remaining character count */}
-          <div className="form-row" style={{ marginLeft: '42%', marginTop:"-15px", color: descriptionLength > 200 ? 'red' : 'black' }}>
-            Remaining characters: {200 - descriptionLength}
-          </div>
+          
 
           <div className="form-row">
             <label htmlFor="receipt-date">Receipt Date:</label>
@@ -177,6 +169,15 @@ const getMinDate = () => {
                 }
               }}
             />
+          </div>
+          <div className="form-row">
+            <label htmlFor="description">Claim Description:</label>
+            {/* Textarea to input the description of the expense claim */}
+            <textarea id="description" value={description} onChange={handleDescriptionChange} maxLength={200} placeholder="Eg. This is description" />
+          </div>
+          {/* Show the remaining character count */}
+          <div className="form-row" style={{ marginLeft: '42%', marginTop: "-15px", color: descriptionLength > 200 ? 'red' : 'black' }}>
+            Remaining characters: {200 - descriptionLength}
           </div>
           {/* Display any errors if present */}
           {error && <p className="error">{error}</p>}
